c4a102c47ed9bdac2ebbe4bb21b46e990c47487c
Vellu <veli-matti.heikkila@helsinki.fi>
2025-06-18 15:09:58 +0300

    renamed variables

--- services/database.js
+++ services/database.js
@@ -5,11 +5,11 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    user: process.env.POSTGRES_USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
     ssl: process.env.SSL ? true : false,
 });
 
