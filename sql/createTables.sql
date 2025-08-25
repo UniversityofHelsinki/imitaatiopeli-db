65ef419db1cf6a229ee508f3639a27691303e447
anttilan <antti.lankinen@helsinki.fi>
2025-08-25 12:14:10 +0300

    Kenttien pituuksia muutettu.

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS GAME_CONFIGURATION (
     answer_randomization BOOLEAN,
     background_info BOOLEAN,
     language_used VARCHAR(255),
-    instructions_for_players VARCHAR(255),
+    instructions_for_players VARCHAR,
     is_research_game BOOLEAN,
-    research_description VARCHAR(255),
+    research_description VARCHAR,
     PRIMARY KEY(config_id)
 );
 
