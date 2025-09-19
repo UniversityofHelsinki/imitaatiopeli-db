1816ec91c7b89f69c1b87a841994996438a5d7c1
Vellu <veli-matti.heikkila@helsinki.fi>
2025-09-19 10:44:22 +0300

    insert or update model_temperature to database

--- sql/createConfiguration.sql
+++ sql/createConfiguration.sql
@@ -1,5 +1,5 @@
 INSERT INTO GAME_CONFIGURATION
-  (
+(
     AI_PROMPT,
     GAME_NAME,
     THEME_DESCRIPTION,
@@ -7,16 +7,18 @@ INSERT INTO GAME_CONFIGURATION
     INSTRUCTIONS_FOR_PLAYERS,
     IS_RESEARCH_GAME,
     RESEARCH_DESCRIPTION,
-    LANGUAGE_MODEL
-  )
-VALUES 
-  (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-    $6,
-    $7,
-   $8
-  ) RETURNING *;
+    LANGUAGE_MODEL,
+    MODEL_TEMPERATURE
+)
+VALUES
+    (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7,
+        $8,
+        $9
+    ) RETURNING *;
