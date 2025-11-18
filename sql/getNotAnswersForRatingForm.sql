d09a47b62966974aef48d0a4a9f094de11dfbc60
anttilan <antti.lankinen@helsinki.fi>
2025-11-18 13:35:13 +0200

    Aikaisemman pelin kysymys jää localstorageen

--- sql/getNotAnswersForRatingForm.sql
+++ sql/getNotAnswersForRatingForm.sql
@@ -1,4 +1,4 @@
-SELECT a.*, q.question_text
+/*SELECT a.*, q.question_text
 FROM ANSWER a
          JOIN QUESTION q ON a.question_id = q.question_id
 WHERE q.game_id = $1
@@ -9,4 +9,13 @@ WHERE q.game_id = $1
              JOIN QUESTION qq ON qq.question_id = jg.question_id
     WHERE qq.game_id = q.game_id
       AND qq.judge_id = q.judge_id
-);
+);*/
+SELECT a.*, q.question_text
+FROM ANSWER a
+         JOIN QUESTION q ON a.question_id = q.question_id
+         LEFT JOIN JUDGE_GUESS jg
+                   ON jg.question_id = q.question_id
+                       AND jg.judge_id   = q.judge_id
+WHERE q.game_id = $1
+  AND q.judge_id = $2
+  AND jg.quess_id IS NULL;
