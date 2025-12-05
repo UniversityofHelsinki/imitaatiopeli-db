5204e47104617e7827c7f7a038ab0dbcfb3a4b1a
anttilan <antti.lankinen@helsinki.fi>
2025-12-05 10:06:30 +0200

    Pelin lopputuloksen näyttäminen pelaajalle

--- sql/getFinalGuessResult.sql
+++ sql/getFinalGuessResult.sql
@@ -4,5 +4,5 @@ SELECT
 FROM JUDGE_FINAL_GUESS jfg
          LEFT JOIN GAME_CONFIGURATION gc
                    ON gc.config_id = jfg.game_id
-WHERE jfg.judge_id = 153
-  AND jfg.game_id = 284;
+WHERE jfg.judge_id = $1
+  AND jfg.game_id = $2;
