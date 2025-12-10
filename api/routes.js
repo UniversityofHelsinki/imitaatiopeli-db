82cd84ad287ffb5ad88090155172223f5b392487
anttilan <antti.lankinen@helsinki.fi>
2025-12-10 09:24:00 +0200

    Ovatko kaikki pelaajat tehneet loppuarvion.

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,13 @@ const { logger } = require('../logger');
 const { execute } = require('../services/database');
 const game = require('../services/game');
 const { savePlayer, pairs } = require('../services/players');
-const { getPlayerById, deleteGame, getJudgeById, getFinalGuessRes } = require('./dbApi');
+const {
+    getPlayerById,
+    deleteGame,
+    getJudgeById,
+    getFinalGuessRes,
+    getHaveAllPlayersEndedGame,
+} = require('./dbApi');
 const crypto = require('node:crypto');
 const { insertAnswer } = require('../services/answer');
 const { insertGuess } = require('../services/guess');
@@ -17,6 +23,7 @@ module.exports = (router) => {
     router.get('/getPlayerById/:playerId', getPlayerById);
     router.get('/getJudgeById/:playerId/:gameId', getJudgeById);
     router.get('/getFinalGuessRes/:judgeId/:gameId', getFinalGuessRes);
+    router.get('/getHaveAllPlayersEndedGame/:gameId', getHaveAllPlayersEndedGame);
 
     router.get('/getJudgeQuestions/:judgeId/:gameId', getJudgeQuestions);
     router.get('/getAIAnswerForQuestion/:aiId/:questionId/:gameId', getAIAnswerForQuestion);
